ba888d817f3ab2463709193a5554382d515e28cc
Howaru1 <Hellwudpecker666@ya.ru>
2023-08-04 16:48:29 +0300

    4. Расстояние между точками в трехмерном пространстве. [#122643  [#122643]]

--- src/test/java/ru/job4j/oop/PointTest.java
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -24,4 +24,40 @@ class PointTest {
         double expected = 1.414;
         assertThat(distance).isCloseTo(expected, offset(0.001));
     }
+
+    @Test
+    public void whenFirst123ToSecond456AndThird123() {
+        Point a = new Point(1, 2, 3);
+        Point b = new Point(4, 5, 6);
+        Point c = new Point(1, 2, 3);
+        double distanceAB = a.distance3d(b);
+        double expectedAB = 5.196;
+        assertThat(distanceAB).isCloseTo(expectedAB, offset(0.001));
+
+        double distanceAC = a.distance3d(c);
+        double expectedAC = 0.0;
+        assertThat(distanceAC).isEqualTo(expectedAC, offset(0.001));
+
+        double distanceBC = b.distance3d(c);
+        double expectedBC = 5.196;
+        assertThat(distanceBC).isCloseTo(expectedBC, offset(0.001));
+    }
+
+    @Test
+    public void whenFirst001ToSecond334AndThird555() {
+        Point a = new Point(1, 2, 1);
+        Point b = new Point(3, 2, 4);
+        Point c = new Point(5, 7, 1);
+        double distanceAB = a.distance3d(b);
+        double expectedAB = 3.605;
+        assertThat(distanceAB).isCloseTo(expectedAB, offset(0.001));
+
+        double distanceAC = a.distance3d(c);
+        double expectedAC = 6.403;
+        assertThat(distanceAC).isEqualTo(expectedAC,offset(0.001));
+
+        double distanceBC = b.distance3d(c);
+        double expectedBC = 6.164;
+        assertThat(distanceBC).isCloseTo(expectedBC, offset(0.001));
+    }
 }
