34bb70998ccfd5f7b4e40365efa7f6736e59d5f2
Howaru1 <Hellwudpecker666@ya.ru>
2024-01-30 17:17:51 +0300

    0.3. Тестирование исключений с junit [#219366 #477466]

--- src/test/java/ru/job4j/ex/FactorialTest.java
+++ src/test/java/ru/job4j/ex/FactorialTest.java
@@ -12,9 +12,16 @@ class FactorialTest {
         IllegalArgumentException exception = assertThrows(
                 IllegalArgumentException.class,
                 () -> {
-                    fact.calc(2);
+                    fact.calc(-1);
 
                 });
         assertThat(exception.getMessage()).isEqualTo("Number could not be less than 0");
+    }
+
+    @Test
+    public void when3To6() {
+        Factorial fact = new Factorial();
+        int rsl = fact.calc(3);
+        assertEquals(rsl, 6);
     }
 }
