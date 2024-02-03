8714e7f8a74ed9340e84449f0445a63f90649bc7
Howaru1 <Hellwudpecker666@ya.ru>
2024-02-03 19:54:46 +0300

    1. Обеспечить бесперебойную работу приложения Tracker [#789]

---
+++ src/main/java/ru/job4j/tracker/actions/ValidateInput.java
@@ -0,0 +1,20 @@
+package ru.job4j.tracker.actions;
+
+import ru.job4j.tracker.ConsoleInput;
+
+public class ValidateInput extends ConsoleInput {
+    @Override
+    public int askInt(String question) {
+        boolean invalid = true;
+        int value = -1;
+        do {
+            try {
+                value = super.askInt(question);
+                invalid = false;
+            } catch (NumberFormatException nfe) {
+                System.out.println("Пожалуйста, введите корректные данные");
+            }
+        } while (invalid);
+        return value;
+    }
+}
